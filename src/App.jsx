be3d5a1164265249e35ccd6671ffc344096d2151
Foad <fwad40901@gmail.com>
2024-08-18 15:16:16 +0330

    replace components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-natural-1">
       <Navbar />
-    </>
+      <Hero />
+      <About />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
   );
 }
