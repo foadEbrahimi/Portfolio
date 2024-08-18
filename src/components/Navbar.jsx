a79505cd443bc150a0b9177b0c6f7d248aa1be9d
Foad <fwad40901@gmail.com>
2024-08-19 00:54:43 +0330

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import menu from '../assets/images/menu.svg';
+import menu from "../assets/images/svgs/menu.svg";
 export default function Navbar() {
   return (
-    <div className="flex shadow-1 p-3 items-center justify-between ">
-      <img src="" className="w-7" alt="logo" />
+    <div className="flex items-center justify-between p-3 px-5 shadow-1 lg:px-10">
       <img src={menu} className="w-7 cursor-pointer" alt="svg" />
+      <img src="" className="w-7" alt="logo" />
     </div>
   );
 }
