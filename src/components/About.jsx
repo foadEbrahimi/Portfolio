a22bb2e397eabede57c81d9bfb8bec0bed603614
Foad <fwad40901@gmail.com>
2024-08-20 22:34:29 +0330

    fix about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Border from "./Border";
 
 import boy from "../assets/images/about/boy.png";
@@ -8,8 +8,15 @@ import doodle3 from "../assets/images/about/doodle3.png";
 import doodle4 from "../assets/images/about/doodle4.png";
 
 import dotsBrand from "../assets/images/svgs/dots-brand.svg";
+import { AboutInfo } from "../constants/index";
 
 export default function About() {
+  const [infoCount, setInfoCount] = useState(0);
+
+  function handlerCount() {
+    setInfoCount((prev) => (prev === 2 ? 0 : prev + 1));
+  }
+
   return (
     <div className="mt-10 bg-white px-5 pt-5 lg:px-10">
       <div className="flex flex-col lg:flex-row lg:justify-between lg:gap-10">
@@ -18,13 +25,15 @@ export default function About() {
             درباره من
           </h2>
           <Border display="flex justify-center w-28 lg:w-28 mx-auto lg:mx-0" />
-          <p className="mt-5 font-iranSans font-semibold text-natural-7 md:mt-6">
-            من فواد ابراهیمی هستم 17 ساله ، برنامه نویس فرانت اند و علاقه مند به
-            یادگیری و کار و 2 پروژه فعال در کامینیوتی فرانت اند. من فواد
-            ابراهیمی هستم 17 ساله ، برنامه نویس فرانت اند و علاقه مند به یادگیری
-            و کار و 2 پروژه فعال در کامینیوتی فرانت اند.
+          <p
+            className={`mt-5 font-iranSans font-semibold text-natural-7 opacity-100 transition-all duration-300 md:mt-6`}
+          >
+            {AboutInfo[infoCount]?.text}
           </p>
-          <button className="mt-5 flex items-center gap-1 font-iranSans font-semibold text-brand-9">
+          <button
+            onClick={handlerCount}
+            className="mt-5 flex animate-bounce items-center gap-1 font-iranSans font-semibold text-brand-9"
+          >
             بیشتر
             <img src={dotsBrand} className="w-5 fill-brand-9" alt="svg" />
           </button>
---
+++ src/constants/index.js
@@ -0,0 +1,36 @@
+import tel from "../assets/images/socials/telegram.png";
+import ins from "../assets/images/socials/instagram.png";
+import links from "../assets/images/socials/linkedin.png";
+
+export const AboutInfo = [
+  {
+    id: 1,
+    text: "            من فواد ابراهیمی هستم 17 ساله ، برنامه نویس فرانت اند و علاقه مند به            یادگیری و کار و 2 پروژه فعال در کامینیوتی فرانت اند. من فواد            ابراهیمی هستم 17 ساله ، برنامه نویس فرانت اند و علاقه مند به یادگیری            و کار و 2 پروژه فعال در کامینیوتی فرانت اند.",
+  },
+  {
+    id: 2,
+    text: "hello world2",
+  },
+  {
+    id: 3,
+    text: "hello world3",
+  },
+];
+
+export const socials = [
+  {
+    id: 1,
+    img: tel,
+    label: "beFrontFace@",
+  },
+  {
+    id: 3,
+    img: links,
+    label: "Foad Ebrahimi",
+  },
+  {
+    id: 2,
+    img: ins,
+    label: "Foad Ebrahimi",
+  },
+];
