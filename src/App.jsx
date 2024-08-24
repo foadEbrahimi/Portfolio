d503b3ab038585332358161df2a5adb47fb4c880
Foad <fwad40901@gmail.com>
2024-08-25 02:41:01 +0330

    App

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Works from "./components/Works";
 import Journy from "./components/Journy";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Testimonial from "./components/Testimonial";
 
 export default function App() {
   return (
-    <div className="bg-natural-1">
+    <div className="bg-natural-1 overflow-hidden">
       <Navbar />
       <Hero />
       <Journy />
       <Works />
       <About />
       <Contact />
+      <Testimonial />
       <br />
       <br />
       <br />
