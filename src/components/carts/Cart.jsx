0f48a79d414c7bdf9f9e8542cf5e57d3e8cc2346
Foad <fwad40901@gmail.com>
2024-08-25 02:40:51 +0330

    change work Cart

--- src/components/carts/Cart.jsx
+++ src/components/carts/Cart.jsx
@@ -1,32 +1,36 @@
 import React from "react";
+
 import img from "./Frame 44 (1).png";
-import user from "../../assets/images/svgs/user.svg";
-import date from "../../assets/images/svgs/date.svg";
+import link from "../../assets/images/svgs/link.svg";
+
 export default function Cart() {
   return (
-    <div className="rounded-xl bg-white p-4 shadow-1">
+    <div className="font-vazir rounded-xl bg-white p-4 shadow-1">
       <img src={img} className="w-full" alt="" draggable={false} />
       <div className="mt-4 space-y-3">
-        <h2 className="font-iranSans text-lg font-bold">
+        <h2 className="flex items-center gap-2 text-lg font-bold">
           اپلیکیشن فیلم و سریال اکتور
+          <a href="link">
+            <img src={link} className="w-5 cursor-pointer" alt="svg" />
+          </a>
         </h2>
-        <p className="min-w-[19rem] text-right font-iranSans text-sm font-semibold leading-[18px] text-natural-7 md:w-full">
+        <p className="min-w-[19rem] text-right text-sm font-semibold leading-[18px] text-natural-7 md:w-full">
           اپلیکیشنی با ظاهری متفاوت و جذاب برای کسانی که در این حوزه فعالیت می
           کنند و برای کاربران خود ارزش قاعل هستند
         </p>
       </div>
-      <div className="mt-5 flex items-center justify-between">
-        <div className="flex items-center gap-1">
-          <img src={user} className="w-5" alt="svg" draggable={false} />
-          <span className="font-iranSans text-sm font-semibold text-natural-3-caption lg:text-body-medium">
-            فواد ابراهیمی
-          </span>
+      <div className="mt-5 flex flex-row-reverse flex-wrap items-center gap-3">
+        <div className="rounded-full bg-warning-6 px-5 py-1 font-morraba text-white">
+          react
         </div>
-        <div className="flex items-center gap-1">
-          <img src={date} className="w-5" alt="svg" draggable={false} />
-          <span className="font-iranSans text-sm font-semibold text-natural-3-caption lg:text-body-medium">
-            1403/05/21
-          </span>
+        <div className="rounded-full bg-warning-6 px-5 py-1 font-morraba text-white">
+          tailwind
+        </div>
+        <div className="rounded-full bg-warning-6 px-5 py-1 font-morraba text-white">
+          redux
+        </div>
+        <div className="rounded-full bg-warning-6 px-5 py-1 font-morraba text-white">
+          react-query
         </div>
       </div>
     </div>
