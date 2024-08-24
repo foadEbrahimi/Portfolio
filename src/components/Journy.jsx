09265802ec6c87be08c9263f2ae015e5211ca743
Foad <fwad40901@gmail.com>
2024-08-25 02:40:26 +0330

    fix fonts

--- src/components/Journy.jsx
+++ src/components/Journy.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import figma from "../assets/images/journy/figma-iso-color.png";
-import rate from "../assets/images/journy/chart-iso-color.png";
 import user from "../assets/images/journy/boy-iso-color.png";
+import rate from "../assets/images/journy/chart-iso-color.png";
 import ellipse from "../assets/images/journy/Ellipse.png";
+import figma from "../assets/images/journy/figma-iso-color.png";
 
 import charactar from "../assets/images/journy/Character_5_Standing 1.png";
 
 export default function Journy() {
   return (
     <div className="relative mt-10 h-fit px-5 py-10 lg:flex lg:items-center lg:justify-end lg:gap-20 lg:bg-white lg:px-10">
-      <div className="grid w-full grid-cols-12 gap-5 lg:w-9/12 md:grid-cols-3">
+      <div className="grid w-full grid-cols-12 gap-5 md:grid-cols-3 lg:w-9/12">
         <div className="col-span-6 space-y-2 rounded-xl p-3 text-center shadow-2 md:col-span-1 lg:bg-natural-1">
           <img
             src={figma}
@@ -24,10 +24,10 @@ export default function Journy() {
             alt="svg"
             draggable={false}
           />
-          <span className="font-iranSans text-xl font-bold text-natural-10-title lg:text-2xl">
+          <span className="font-vazir text-xl font-bold text-natural-10-title lg:text-2xl">
             مـدت فـعـالـیـت
           </span>
-          <p className="font-iranSans text-body-small font-semibold text-natural-7">
+          <p className="font-vazir text-body-small font-semibold text-natural-7">
             6 ماه یادگیری حرفه ای
           </p>
         </div>
@@ -44,10 +44,10 @@ export default function Journy() {
             alt="svg"
             draggable={false}
           />
-          <span className="font-iranSans text-xl font-bold text-natural-10-title lg:text-2xl">
+          <span className="font-vazir text-xl font-bold text-natural-10-title lg:text-2xl">
             مـدت فـعـالـیـت
           </span>
-          <p className="font-iranSans text-body-small font-semibold text-natural-7">
+          <p className="font-vazir text-body-small font-semibold text-natural-7">
             6 ماه یادگیری حرفه ای
           </p>
         </div>
@@ -64,10 +64,10 @@ export default function Journy() {
             alt="svg"
             draggable={false}
           />
-          <span className="font-iranSans text-xl font-bold text-natural-10-title lg:text-2xl">
+          <span className="font-vazir text-xl font-bold text-natural-10-title lg:text-2xl">
             مـدت فـعـالـیـت
           </span>
-          <p className="font-iranSans text-body-small font-semibold text-natural-7">
+          <p className="font-vazir text-body-small font-semibold text-natural-7">
             6 ماه یادگیری حرفه ای
           </p>
         </div>
