63399d1efcb1cd0886d90f669ee085a0a2c2456c
Foad <fwad40901@gmail.com>
2024-08-26 02:55:50 +0330

    fix cart

--- src/components/carts/TestimonialCart.jsx
+++ src/components/carts/TestimonialCart.jsx
@@ -3,13 +3,12 @@ import React from "react";
 export default function TestimonialCart() {
   return (
     <div className="mt-10 rounded-lg p-5 shadow-2">
-      <h2 className="font-vazir text-center text-lg">
+      <h2 className="text-center font-vazir text-lg">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
         از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
         سطرآنچنان که لازم است
       </h2>
       <div className="mt-5 flex flex-col items-center gap-1">
-        <span className="font-vazir text-xl font-extrabold">فواد ابراهیمی</span>
         <span className="font-vazir font-semibold text-natural-7">
           tetherrate.net
         </span>
